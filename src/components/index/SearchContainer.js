7af8ca9d7cb7373a03d52ee25aac760e40c439fa
stalliardo187 <darrenstallard27@hotmail.com>
2023-01-24 15:33:16 +0000

    Added some get requests to the api file

--- src/components/index/SearchContainer.js
+++ src/components/index/SearchContainer.js
@@ -5,13 +5,20 @@ import TextField from '@mui/material/TextField';
 import Typography from '@mui/material/Typography';
 import Paper from '@mui/material/Paper';
 import Button from '@mui/material/Button';
+import { searchServers } from '../../services/API/serverAPIs';
 
 const SearchContainer = () => {
     const [searchButtonDisabled, setSearchButtonDisabled] = useState(true);
-    const [searcTerm, setSearchTerm] = useState(""); 
+    const [searchTerm, setSearchTerm] = useState(""); 
 
     const onSubmit = (e) => {
         e.preventDefault();
+        
+        searchServers(searchTerm).then((response) => {
+            console.log("response = ", response.data);
+        }).catch(e => {
+            console.log("error getting server. Error: ", e);
+        })
     }
 
     const handleChange = (e) => {
@@ -19,15 +26,15 @@ const SearchContainer = () => {
     }
 
     useEffect(() => {
-        setSearchButtonDisabled(searcTerm.length < 3);
-    }, [searcTerm]);
+        setSearchButtonDisabled(searchTerm.length < 3);
+    }, [searchTerm]);
 
     return (
         <Box component={Paper} sx={{ textAlign: "left", padding: "20px" }}>
             <form onSubmit={onSubmit}>
                 <Typography variant="subtitle1">Search Servers</Typography>
                 <TextField onChange={handleChange} fullWidth />
-                <Button variant="contained" sx={{ mt: "10px" }} disabled={searchButtonDisabled}>Search</Button>
+                <Button variant="contained" type="submit" sx={{ mt: "10px" }} disabled={searchButtonDisabled}>Search</Button>
             </form>
         </Box>
     )
