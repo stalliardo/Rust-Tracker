c9ed24979d3f81a2814123702ad2fc58461cfcab
stalliardo187 <darrenstallard27@hotmail.com>
2023-01-24 13:54:26 +0000

    Created index route and component.

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ import { BrowserRouter, Routes, Route, } from "react-router-dom";
 import Navbar from './components/navbar/Navbar';
 import Auth from './components/auth/Auth';
 import PageNotFound from './components/errors/PageNotFound';
+import Index from './routes/Index';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
@@ -18,8 +19,8 @@ root.render(
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<App />}>
+            <Route index element={<Index />} />
             <Route path="*" element={<PageNotFound />}/>
-            {/* <Route index element={<App />} /> */}
             <Route path="/auth" element={<Auth />} />
           </Route>
         </Routes>
---
+++ src/routes/Index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import SearchContainer from '../components/index/SearchContainer'
+
+const Index = () => {
+  return (
+    <div>
+        <SearchContainer />
+    </div>
+  )
+}
+
+export default Index
