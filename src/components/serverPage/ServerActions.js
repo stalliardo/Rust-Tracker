35b053629eff6f92ec458212ee46f4f7b467aea2
stalliardo187 <darrenstallard27@hotmail.com>
2023-01-26 16:09:02 +0000

    Moved the connect to server link into the registeredUserActions comp

--- src/components/serverPage/ServerActions.js
+++ src/components/serverPage/ServerActions.js
@@ -1,31 +1,22 @@
-import { Box, Typography, Link } from '@mui/material'
-import React from 'react'
+import React from 'react';
+import { Box, Typography, Link } from '@mui/material';
 import RegisteredUserActions from './RegisteredUserActions'
 
 const ServerActions = ({data}) => {
     return (
         <Box>
             <Typography variant="h6" color="primary" sx={{ textDecoration: "underline" }}>Registered user perks</Typography>
-
-            {/* TODO */}
             <Typography variant="subtitle1">
                 Registered users have access to more advanced features. Such as, adding a server to their favourites list, adding notes for a server,
                 alerts for status changes, player count etc. Register now to activate these features and gain an advantage on the competition.
             </Typography>
             {/* TODO decide whether to use a link or a large button */}
-            <Link href="/auth">Register</Link>
-
-            {/* TEST below on gaming pc link also needs to be dynamic*/}
-            {/* <Link href="steam://connect/168.100.161.157:28015">Connect</Link> */}
-            
+            <Link href="/auth" sx={{textDecoration: "none"}}>Register</Link>
             <Typography mt="20px" variant="h6" color="primary" sx={{ textDecoration: "underline" }}>Socials</Typography>
-
-            <Typography>Website: {data.attributes.details.rust_url ? <Box component="span"><Link href={data.attributes.details.rust_url}>{data.attributes.details.rust_url}</Link></Box> : "Not Found!"}</Typography>
-
+            <Typography>Website: {data.attributes.details.rust_url ? <Box component="span"><Link sx={{textDecoration: "none"}} href={data.attributes.details.rust_url}>{data.attributes.details.rust_url}</Link></Box> : "Not Found!"}</Typography>
             <RegisteredUserActions />
-
         </Box>
     )
-}
+};
 
-export default ServerActions
+export default ServerActions;
