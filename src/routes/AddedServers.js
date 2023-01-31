01f99a37b7f0a353485a71a700fd6a4723275ceb
stalliardo187 <darrenstallard27@hotmail.com>
2023-01-31 17:38:53 +0000

    navigated to server when the view icon is clicked.

--- src/routes/AddedServers.js
+++ src/routes/AddedServers.js
@@ -1,26 +1,25 @@
 import { Box, Typography, Paper } from '@mui/material';
 import React, { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 import ExtendableTable from '../components/table/ExtendableTable';
 
 const AddedServers = () => {
   const servers = useSelector(state => state.user.servers);
+  const navigate = useNavigate();
 
   console.log("servers = ", servers);
 
-  const [tableData, setTableData] = useState({ head: ["Server name", "Notes added"], rows: [] });
+  const [tableData, setTableData] = useState({ head: ["Server name", "Notes added", ""], rows: [] });
 
   useEffect(() => {
-    // Loop through the servers and set the table data
-
     if (servers.length) {
       const rowData = [];
 
       servers.forEach((server) => {
-        console.log("Server itme = ", server);
         rowData.push({
           serverName: server.name,
-          notesAdded: server.notes !== "",
+          notesAdded: server.notes !== "" ? "True" : "False",
           id: server.id
         })
       });
@@ -30,12 +29,29 @@ const AddedServers = () => {
     }
   }, [servers]);
 
+  const handleViewClicked = (row) => {
+    navigate(`/server/${row.id}`);
+  }
+
+  const handleDeleteClicked = (row) => {
+    console.log("delete clicked. Row = ", row);
+  }
+
   return (
     <Box component={Paper} sx={{ textAlign: "left", padding: "20px" }}>
       <Typography variant='h4' color="primary">Saved Servers</Typography>
       {
         servers.length > 0 ?
-          <ExtendableTable data={tableData} /> :
+          <ExtendableTable 
+            data={tableData} 
+            viewButton={true} 
+            viewButtonTooltipText="View server"
+            handleViewClicked={handleViewClicked}
+            deleteButton={true} 
+            deleteButtonTooltipText="Delete server"
+            handleDelete={handleDeleteClicked}
+            rowClickingDisabled={true}
+            /> :
           <Typography>Your saved servers will be displayed here.</Typography>
       }
     </Box>
