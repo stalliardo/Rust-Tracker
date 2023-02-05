4a361ab71cd168ec604ec8c246c22c310668b59b
darren <stalliardo187@gmail.com>
2023-02-05 20:46:29 +0000

    Extracted the ADMIN_ID from the environment variables and used to determine whether or not to run the function

--- functions/index.js
+++ functions/index.js
@@ -7,61 +7,37 @@ import { generateApiQueryFromAlerts } from './database.js';
 admin.initializeApp();
 
 export const refreshPlayerStatus = functions.https.onRequest(async (req, res) => {
+  const userId = req.query.userId;
+  const adminId = process.env.ADMIN_ID;
 
-  // Will need to pass my id via the params
+  if (userId === adminId) {
+    const promises = await generateApiQueryFromAlerts();
+    const apiData = [];
 
-  // const writeResult = await admin.firestore().collection('alerts').add({
-  //   playerName: "Darren",
-  //   playerId: "1120601975",
-  //   serverId: "5967676",
-  //   alertType: "email",
-  //   isOnline: null
-  // });
+    if (promises.length) {
+      const response = await Promise.all(promises);
+      response.forEach((r, index) => {
+        apiData.push({
+          meta: r.data.included[0].meta,
+          serverId: r.data.included[0].id,
+          playerId: r.data.data.id
+        });
+      })
 
-  //TODO call process.env.ADMIN_ID
+      const alerts = await admin.firestore().collection("alerts").get();
 
-  const promises = await generateApiQueryFromAlerts();
-  const apiData = [];
+      alerts.forEach(async (alert) => {
+        const match = apiData.find(data => (data.serverId === alert.data().serverId && data.playerId === alert.data().playerId));
 
-  if (promises.length) {
-    const response = await Promise.all(promises);
-    response.forEach((r, index) => {
-      apiData.push({
-        meta: r.data.included[0].meta,
-        serverId: r.data.included[0].id,
-        playerId: r.data.data.id
-      });
-    })
-
-    const alerts = await admin.firestore().collection("alerts").get();
-
-    alerts.forEach(async (alert) => {
-      // find the array with the matching playerid and serverid. then update that record
-      const match = apiData.find(data => (data.serverId === alert.data().serverId && data.playerId === alert.data().playerId));
-      await alert.ref.update({ isOnline: match.meta.online })
-    })
+        //TODO set the isNew field if isNew is true.
+        await alert.ref.update({ isOnline: match.meta.online })
+      })
+    } else {
+      return res.json({ message: "No alerts found" })
+    }
   } else {
-    return res.json({message: "No alerts found"})
+    return res.status(403).json({ message: "The provided user id is not authorized to perform that action." })
   }
-
-
 });
 
-// exports.sendAlert = functions.firestore.document('/alerts/{documentId}')
-//   .onCreate((snap, context) => {
-//     // Grab the current value of what was written to Firestore.
-//     const original = snap.data();
-//     console.log('original = ', original);
-
-
-//     // Access the parameter `{documentId}` with `context.params`
-//     functions.logger.log('alert with id', context.params.documentId, " added");
-
-
-//     // You must return a Promise when performing asynchronous tasks inside a Functions such as
-//     // writing to Firestore.
-//     // Setting an 'uppercase' field in Firestore document returns a Promise.
-
-
-    // return snap.ref.set({ hasBeenTriggered: true }, { merge: true });
-//   });
+// TODO - sendAlert to either email, site or sms...
